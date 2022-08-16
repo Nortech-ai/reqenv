d755758db06363f35b6ae02d69bbe3ccaa39a96f
João Adriano <joao@nortech.ai>
2022-08-16 14:16:58 +0100

    V1

---
+++ README.md
@@ -0,0 +1,14 @@
+# Reqenv
+
+```
+Usage: reqenv <variables...>
+
+Require environment variables from your npm scripts with a self evident error message
+
+Arguments:
+  variables      What variables you want to check e.g AWS_KEY WORKSPACE
+
+Options:
+  -V, --version  output the version number
+  -h, --help     display help for command
+```
---
+++ index.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env ts-node --swc
+
+import { Command } from "commander";
+import { readFileSync } from "fs";
+import { bold, red } from "kleur";
+import type { IPackageJson } from "package-json-type";
+import { join } from "path";
+
+const program = new Command();
+
+const packageDetails = getCurrentPackageDetails();
+program
+  .version(packageDetails.version!)
+  .name(packageDetails.name!.replace("@nortech/", ""))
+  .description(packageDetails.description!);
+
+program
+  .argument(
+    "<variables...>",
+    "What variables you want to check e.g AWS_KEY WORKSPACE"
+  )
+  .action((variables: string[]) => {
+    const missingVariables = [] as string[];
+
+    for (const variable of variables) {
+      if (!process.env[variable]) {
+        missingVariables.push(variable);
+      }
+    }
+
+    if (missingVariables.length > 0) {
+      console.error(
+        red(
+          `${bold(missingVariables.join(", "))} - Environment ${
+            missingVariables.length > 1 ? "variables are" : "variable is"
+          } missing and must be defined `
+        )
+      );
+      process.exit(1);
+    }
+  });
+program.parse();
+
+function getCurrentPackageDetails() {
+  return JSON.parse(
+    readFileSync(join(__dirname, "package.json"), "utf-8")
+  ) as IPackageJson;
+}
